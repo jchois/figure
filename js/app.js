b06e31ebb656287564e1eeac63aee473f63b49f5
juliana <choisjuliana@gmail.com>
2018-06-19 13:32:40 -0500

    html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function changeLayout() {
+    document.getElementById("hours-min").style.display = "flex";
+    document.getElementById("hours-min").style.flexDirection = "column";
+    document.getElementById("hours-min").style.margin = "-10px 0 0 0";
+    document.getElementById("minutes").style.margin = "10px 0 15px 0";
+    document.getElementById("box").style.height = "350px";
+  }
+
+
+
