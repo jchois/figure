f622542deabaf2b70625e561f6cd6c53e4fe3860
juliana <choisjuliana@gmail.com>
2018-06-19 17:10:33 -0500

    submit

--- js/app.js
+++ js/app.js
@@ -6,5 +6,25 @@ function changeLayout() {
     document.getElementById("box").style.height = "350px";
   }
 
+function submit() {
+    var h = document.getElementById('input-h');
+    var m = document.getElementById('input-m');
+
+    if(h.value == 12 && m.value == 0){
+        document.getElementById("result").innerHTML = "15&ordm";
+    }
+    
+    
+    // if (h <0 || m < 0 || h >12 || m > 60){
+    //     alert("Error");
+    // }
+
+    /*
+    if (h == 12) h = 0;
+    if (m == 60) m = 0;
+    */
+}
+
+
 
 
