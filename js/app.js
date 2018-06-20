f1a0ad2e7dadde7a241dcac2f2a6fcfcbbd9a79f
juliana <choisjuliana@gmail.com>
2018-06-20 10:35:22 -0500

    valid

--- js/app.js
+++ js/app.js
@@ -10,11 +10,22 @@ function submit() {
     var h = document.getElementById('input-h');
     var m = document.getElementById('input-m');
 
-    if(h.value == 12 && m.value == 0){
+    if(h.value == 0 || h.value > 12 || m.value == 0 || m.value > 60 ){
+
+        alert("Error");
+        document.getElementById("result").innerHTML = "0&ordm";
+        document.getElementById("tittle-angle").innerHTML = "Add hours and minutes to get an angle";
+    }
+    else{
+        document.getElementById("tittle-angle").innerHTML = "At " + h.value + " Hours with " + m.value + " Minutes there's an angle of: " ;
         document.getElementById("result").innerHTML = "15&ordm";
     }
     
-    
+    // if(h.value != 0 && h.value < 12|| m.value != 0 ){
+    //    document.getElementById("result").innerHTML = "15&ordm";
+    //}
+
+    // h.value <= 12 || m.value <= 60
     // if (h <0 || m < 0 || h >12 || m > 60){
     //     alert("Error");
     // }
